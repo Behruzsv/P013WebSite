d9fbdcd0292c62fe2f339a03f6e0d6a74d00d5e3
Behruzsv <126698226+Behruzsv@users.noreply.github.com>
2023-04-24 23:04:42 +0300

    Add project files.

---
+++ P013WebSite/Areas/Admin/Controllers/MainController.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNetCore.Authorization; // Oturum açmayı gerekli kılan kütüphane Authorize
+using Microsoft.AspNetCore.Mvc;
+
+namespace P013WebSite.Areas.Admin.Controllers
+{
+    [Area("Admin"), Authorize] // Bir controller a Authorize ü uygukarsak controller içerisindeki bütün actionlara erişimi engellemiş oluruz. Sadece oturum açan kullanıcılar ekranları görebilir.
+    public class MainController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
